9554e100dee78c0e4dc55aa88a86775ef325b4f5
ajlopez <webmaster@ajlopez.com>
2014-06-22 07:56:37 -0300

    Guard functions in trigger; internal refactor

--- test/firefn.js
+++ test/firefn.js
@@ -25,3 +25,19 @@ exports['fire a trigger with two functions'] = function (test) {
     test.equal(calls2, 1);
 };
 
+exports['fire a trigger with a guard functions'] = function (test) {
+    var calls = 0;
+    var sm = ss.stateMachine('OffHook');
+    sm.state('OffHook').when('CallDialed', function () { calls++; if (calls <= 1) return false; }, 'Ringing');
+    
+    test.equal(sm.getState(), 'OffHook');
+    
+    sm.fire('CallDialed');
+    test.equal(sm.getState(), 'OffHook');
+    test.equal(calls, 1);
+    
+    sm.fire('CallDialed');
+    test.equal(sm.getState(), 'Ringing');
+    test.equal(calls, 2);
+};
+
