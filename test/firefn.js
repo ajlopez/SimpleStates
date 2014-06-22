29428ad2f4475ffb2243fdd6c0b89e998845c46b
ajlopez <webmaster@ajlopez.com>
2014-06-22 08:03:17 -0300

    Multiple branch for same trigger, with guard functions

--- test/firefn.js
+++ test/firefn.js
@@ -41,3 +41,27 @@ exports['fire a trigger with a guard functions'] = function (test) {
     test.equal(calls, 2);
 };
 
+exports['fire the second trigger with a failed guard functions for first trigger'] = function (test) {
+    var calls = 0;
+    var firstfired = 0;
+    var secondfired = 0;
+    
+    var sm = ss.stateMachine('OffHook');
+    sm.state('OffHook')
+        .when('CallDialed', 
+            function () { calls++; if (calls <= 1) return false; },
+            function () { firstfired++; },
+            'Ringing')
+        .when('CallDialed', 
+            function () { secondfired++; },
+            'Ringing');
+    
+    test.equal(sm.getState(), 'OffHook');
+    
+    sm.fire('CallDialed');
+    test.equal(sm.getState(), 'Ringing');
+    test.equal(firstfired, 0);
+    test.equal(secondfired, 1);
+    test.equal(calls, 1);
+};
+
